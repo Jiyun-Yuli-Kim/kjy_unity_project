1adcdb3ad445174bda4772ab273bec33e0a72683
Jiyun <juliyooni@gmail.com>
2024-12-26 21:40:36 +0900

    241226 21:40 저장

--- kjy_unity_project/Assets/CSV/DialogueLoader.cs
+++ kjy_unity_project/Assets/CSV/DialogueLoader.cs
@@ -3,53 +3,49 @@ using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
+using UnityEngine.Networking;
 
 public class DialogueLoader : MonoBehaviour
 {
     private const string KindDialogue =
-        "https://docs.google.com/spreadsheets/d/13v8TbGoXz7ywTqHxwMzNohhG4DBODOhcLutcQ3UUJLk/edit?gid=0#gid=0";
+        "https://docs.google.com/spreadsheets/d/e/2PACX-1vR8uX0llujfHBKqUOCZ92p80anVPJEmy9HNbHRY5buq3ICGfkflCrZvvJMj6yy6etR6dDfayBMg56N1/pub?gid=0&single=true&output=csv";
 
     private const string IdolDialogue =
-        "https://docs.google.com/spreadsheets/d/1DS6qp310IcUBQu8Mw01p27nPV_-LGkUwwxcrXMH3nk0/edit?gid=0#gid=0";
+        "https://docs.google.com/spreadsheets/d/e/2PACX-1vT14RTV0DSuTbon86CiecSde7DullmFy3g9reMeRJCoyhvakMuQgtfVwagq8N47fMl1neSdooXMpNsK/pub?gid=0&single=true&output=csv";
         
     public string[,] DialogueData { get; private set; }
 
     private void Awake()
     {
-        DialogueData = LoadCSV();
+        StartCoroutine(DownLoadRoutine(KindDialogue));
 
-        for (int i = 1; i < DialogueData.GetLength(0); i++)
-        {
-            for (int j = 0; j < DialogueData.GetLength(1); j++)
-            {
-                Debug.Log(DialogueData[i, j]);
-            }
-        }
     }
 
-    public string[,] LoadCSV()
+    IEnumerator DownLoadRoutine(string urlPath)
     {
-        string path = Application.dataPath + "/CSV/DataTable";
+        UnityWebRequest request = UnityWebRequest.Get(urlPath);
+        yield return request.SendWebRequest();
 
-        if (!Directory.Exists(path))
+        if (request.result == UnityWebRequest.Result.Success)
         {
-            Debug.LogError("경로가 없습니다.");
-            return null;
+            string recievedData = request.downloadHandler.text;
+            ProcessCSV(recievedData);
         }
 
-        if (!File.Exists(path + "/DialogueCSV.csv"))
+        else
         {
-            Debug.LogError("파일이 없습니다.");
-            return null;
+            Debug.Log("데이터를 로드할 수 없습니다");
         }
+        
+    }
 
-        string file = File.ReadAllText(path + "/DialogueCSV.csv");
-
+    private string[,] ProcessCSV(string data)
+    {
         // 행별로 나눔
-        string[] lines = file.Split('\n');
-
+        string[] lines = data.Split('\n');
+    
         string[,] DialogueTable = new string[lines.Length, lines[0].Split(',').Length];
-
+    
         for (int i = 1; i < lines.Length; i++)
         {
             string[] values = lines[i].Split(',');
@@ -58,7 +54,34 @@ public class DialogueLoader : MonoBehaviour
                 DialogueTable[i, j] = values[j];
             }
         }
-
+    
         return DialogueTable;
     }
+
+    private void ShowCSVData()
+    {
+        DialogueData = ProcessCSV();
+        
+        for (int i = 1; i < DialogueData.GetLength(0); i++)
+        {
+            for (int j = 0; j < DialogueData.GetLength(1); j++)
+            {
+                Debug.Log(DialogueData[i, j]);
+            }
+        }
+    }
 }
+
+// string path = Application.dataPath + "/CSV/DataTable";
+//     
+// if (!Directory.Exists(path))
+// {
+//     Debug.LogError("경로가 없습니다.");
+//     return null;
+// }
+//     
+// if (!File.Exists(path + "/DialogueCSV.csv"))
+// {
+//     Debug.LogError("파일이 없습니다.");
+//     return null;
+// }
