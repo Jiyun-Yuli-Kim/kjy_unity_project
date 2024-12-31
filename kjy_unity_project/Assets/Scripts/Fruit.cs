c5864029bd88c468a10dd70e5e2747d7eee43b1a
Jiyun-Juli-Kim <juliyooni@gmail.com>
2025-01-01 00:29:25 +0900

    250101 00:29 저장

--- kjy_unity_project/Assets/Scripts/Fruit.cs
+++ kjy_unity_project/Assets/Scripts/Fruit.cs
@@ -1,17 +1,38 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Fruit : MonoBehaviour
 {
     private Rigidbody _rb;
     private Collider _col;
+    
+    private PlayerInput _input;
+    
+    private bool _isPickupable = false;
 
     void Awake()
     {
         _rb = GetComponent<Rigidbody>();
         _col = GetComponent<Collider>();
+    }
+
+    void Update()
+    {
+        CheckPickupable();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            // _isPickupable = true 주울 수 있는 상태가 된다
+            // _isPickupable == true이고 Trigger 눌렸을 때(업데이트에서 처리)
+            // PickUpFruit()
+        }
     }
 
     private void OnCollisionStay(Collision other)
@@ -22,9 +43,30 @@ public class Fruit : MonoBehaviour
         }
     }
 
+    private void CheckPickupable()
+    {
+        if (!_isPickupable)
+        {
+            return;
+        }
+        
+        else if (_isPickupable)
+        {
+            if (_input.actions["Trigger"].WasPressedThisFrame())
+            {
+                PickUpFruit();
+            }
+        }
+    }
+
     public void FruitFall()
     {
         _rb.constraints = RigidbodyConstraints.None;
+    }
+
+    public void PickUpFruit()
+    {
+        // 직접적으로 과일을 줍는 로직
     }
 
 }
---
+++ kjy_unity_project/Assets/Scripts/Inventory.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    private List<GameObject> _inventory = new List<GameObject>();
+    private int _curItemCount = 0; 
+    private int _maxItemCount = 30;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
