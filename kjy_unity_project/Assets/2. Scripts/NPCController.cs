24fcb7a14184d01a789f98d98a36230690f7d99f
Jiyun-Juli-Kim <juliyooni@gmail.com>
2025-01-13 19:08:24 +0900

    250113 19:08 저장

--- kjy_unity_project/Assets/2. Scripts/NPCController.cs
+++ kjy_unity_project/Assets/2. Scripts/NPCController.cs
@@ -13,28 +13,48 @@ public class NPCController : MonoBehaviour, IInteractable, ITalkable
     
     // 랜덤이동 구현용
     [SerializeField] public float UpdateInterval;
+    private float _curTime;
     [SerializeField] NavMeshAgent _agent;
     // 테스트용 목적지
-    [SerializeField] public Transform target;
+    // [SerializeField] float _speed;
     
     public event UnityAction OnInteract;
     public event UnityAction OnInteractEnd;
 
     private string[,] _dialogueData;
     
-    
-    
     public void Start()
     {
         DialogueSystem.Instance.OnDataLoaded.AddListener(SetData);
-        _agent.SetDestination(target.position);
+        _curTime = UpdateInterval;
+        // _agent.speed = _speed;
     }
     
     void Update()
     {
-     
+        _curTime += Time.deltaTime;
+        if (_curTime >= UpdateInterval)
+        {
+            Vector3 randpos = GetRandPosOnNavMesh();
+            _agent.SetDestination(randpos);
+            _curTime = 0;
+        }
     }
 
+    private Vector3 GetRandPosOnNavMesh()
+    {
+        Vector3 randomPos = Random.insideUnitSphere*40;
+        randomPos += transform.position;
+        NavMeshHit navHit;
+        if (NavMesh.SamplePosition(randomPos, out navHit, 20f, NavMesh.AllAreas))
+        {
+            return navHit.position;
+        }
+        else
+        {
+            return transform.position;
+        }
+    }
 
     public void Interact()
     {
